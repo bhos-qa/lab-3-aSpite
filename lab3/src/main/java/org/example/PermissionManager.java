52b2e094c27cc0104cd32c8ae975d2ae2c22d0e5
aSpite <spite.moriarty@gmail.com>
2024-09-24 07:22:55 +0400

    add project

---
+++ lab3/src/main/java/org/example/PermissionManager.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class PermissionManager {
+    private PermissionLevel mCurrentLevel = PermissionLevel.USER;
+
+    public String getRoleName() {
+        return mCurrentLevel.name();
+    }
+
+    public void setRoleName(PermissionLevel level) {
+        mCurrentLevel = level;
+    }
+}
